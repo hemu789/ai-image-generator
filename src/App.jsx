6f4603f5a045d61a04b05e1a309a3cb865a8f85e
hemu789 <87188130+hemu789@users.noreply.github.com>
2024-02-14 20:33:37 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ImageGenerator from './Components/ImageGenerator/imageGenerator.jsx'
+function App() {
+
+  return (
+    <>
+      <ImageGenerator />
+    </>
+  );
+}
+
+export default App
